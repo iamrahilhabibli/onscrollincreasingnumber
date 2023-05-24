b50bf7e65fa2ddbf7ae32c07d3bec102a23dd0ce
Habibli Rahil <RahilaH@code.local>
2023-05-24 15:50:35 +0400

    on scroll incrementing numbers

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", () => {
+  function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+      current = start,
+      range = end - start,
+      increment = end > start ? 1 : -1,
+      step = Math.abs(Math.floor(duration / range)),
+      timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current;
+        if (current == end) {
+          clearInterval(timer);
+        }
+      }, step);
+  }
+  counter("count1", 0, 220, 3000);
+  counter("count2", 0, 25, 3000);
+  counter("count3", 0, 3, 3000);
+});
